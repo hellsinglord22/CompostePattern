c235b0dd5e3a5df4d6b20ecd3fc7f51cd015ccf1
H <heshamscorpe55@gmail.com>
2015-10-14 00:52:55 +0200

    create a MenuItem
    a menu item can be added to a restaurant menu

---
+++ src/MenuItem.java
@@ -0,0 +1,69 @@
+
+public class MenuItem extends MenuComponent {
+
+	/// Attributes ///
+	private String name;
+	private String description;
+	private boolean vegetarian;
+	private double price;
+
+	/***
+	 * @param name
+	 *            the name of the item
+	 * @param description
+	 *            a small description of the item
+	 * @param vegetarian
+	 *            is the item contains meet or not
+	 * @param price
+	 *            the price of the item
+	 */
+	public MenuItem(String name, String description, boolean vegetarian, double price) {
+		this.name = name;
+		this.description = description;
+		this.vegetarian = vegetarian;
+		this.price = price;
+	}
+
+	/**
+	 * @return the name of the item
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return a small description of the item
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @return true of the item doesn't contain meat and false otherwise
+	 */
+	public boolean isVegetarian() {
+		return vegetarian;
+	}
+
+	/**
+	 * @return the price of the item
+	 */
+	public double getPrice() {
+		return price;
+	}
+
+	/// Custom methods ///
+	/**
+	 * print the information of the item
+	 */
+	public void print() {
+		System.out.print("  " + getName());
+		if (isVegetarian()) {
+			System.out.print("(v)");
+		}
+		System.out.println(", " + getPrice());
+		System.out.println("     -- " + getDescription());
+
+	}
+
+}
